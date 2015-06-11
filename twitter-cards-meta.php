dcfc594cfd7787f3516ff45ebe6539866aa73112
M Asif Rahman <asif2bd@gmail.com>
2015-06-11 17:29:18 -0400

    Minor 2.1.5

--- twitter-cards-meta.php
+++ twitter-cards-meta.php
@@ -3,7 +3,7 @@
  * Plugin Name: Twitter Cards Meta
  * Plugin URI: https://wpdeveloper.net/go/TCM
  * Description: The Only Complete Twitter Cards Plugin in WordPress. Supports Product Card & Summary Card with Large Image. Advance Automated settings. Works with WooCommerce as well.
- * Version: 2.1.4
+ * Version: 2.1.5
  * Author: WPDeveloper.net
  * Author URI: https://wpdeveloper.net/
  * License: GPLv2+
@@ -340,9 +340,9 @@ if ( current_user_can( 'install_plugins' ) )
 	global $current_user ;
         $user_id = $current_user->ID;
         /* Check that the user hasn't already clicked to ignore the message */
-	if ( ! get_user_meta($user_id, 'twcm_ignore_notice214') ) {
+	if ( ! get_user_meta($user_id, 'twcm_ignore_notice215') ) {
         echo '<div class="updated"><p>';
-        printf(__('<strong>[Howdy!]</strong> Its <a href="http://asif.im" target="_blank">Asif</a> here! Do you know over <strong>5000</strong> Websites using <strong><a href="https://wpdeveloper.net/go/TCM" target="_blank">Twitter Cards Meta</a></strong>! Amazing right? If you are happy with us why not give us a little heads up, <a href="https://wpdeveloper.net/go/twmc-rating" target="_blank"><b>review us in WordPress.org</b></a>, it takes only few seconds. If you have any question or need any help, dont hesitate to <a href="https://wpdeveloper.net/go/TCM-Contact" target="_blank">contact us</a>.</strong> <a href="%1$s">[Hide Notice]</a>'),  admin_url( 'admin.php?page=twitter-cards-meta&twcm_nag_ignore=0' ));
+        printf(__('<b>[FYI]</b> Do you know in a single website you could have <strong>multiple <a href="https://wpdeveloper.net/go/TCM" target="_blank">Twitter Card</a></strong> types? Like you use <strong><a href="http://wpdeveloper.net/go/TCM-SCLI" target="_blank">Summary Card with Large Image</a></strong> as default card, but when you post image you could use <strong><a href="https://wpdeveloper.net/go/TCM" target="_blank">Photo Card</a></strong> or even <strong><a href="https://wpdeveloper.net/go/TCM-GC" target="_blank">Gallery Card</a> </strong>and when you are talking about a product, use <strong><a href="https://wpdeveloper.net/go/TCM-PC" target="_blank">Product Card</a>. </strong>Makes sense, right? Your audience will love it! <a href="%1$s">[Hide]</a>'),  admin_url( 'admin.php?page=twitter-cards-meta&twcm_nag_ignore=0' ));
         echo "</p></div>";
 	}
     }
@@ -355,7 +355,7 @@ function twcm_nag_ignore() {
         $user_id = $current_user->ID;
         /* If user clicks to ignore the notice, add that to their user meta */
         if ( isset($_GET['twcm_nag_ignore']) && '0' == $_GET['twcm_nag_ignore'] ) {
-             add_user_meta($user_id, 'twcm_ignore_notice214', 'true', true);
+             add_user_meta($user_id, 'twcm_ignore_notice215', 'true', true);
 	}
 }
 
